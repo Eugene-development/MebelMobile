4c225767b0ced86b8ad749b0b4b055196111c200
indexpro24@gmail.com <indexpro24@gmail.com>
2021-05-31 09:30:05 +0300

    git init

--- src/nuxt.config.js
+++ src/nuxt.config.js
@@ -1,4 +1,3 @@
-
 export default {
   mode: 'universal',
 
@@ -10,35 +9,33 @@ export default {
   ** Headers of the page
   */
   head: {
-    title: process.env.npm_package_name || '',
+    title: 'Кухонные гарнитуры и кухни на заказ по размерам заказчика',
     meta: [
-      { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: process.env.npm_package_description || '' }
+      {charset: 'utf-8'},
+      {name: 'viewport', content: 'width=device-width, initial-scale=1'},
+      {hid: 'description', name: 'description', content: 'Изготовление и продажа кухонных гарнитуров под заказ по индивидуальным размерам помещений'}
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+      {rel: 'icon', type: 'image/x-icon', href: '/favicon.ico'}
     ]
   },
   /*
   ** Customize the progress-bar color
   */
-  loading: { color: '#fff' },
+  loading: {color: '#fff'},
   /*
   ** Global CSS
   */
-  css: [
-  ],
+  css: [],
   /*
   ** Plugins to load before mounting the App
   */
-  plugins: [
-  ],
+  plugins: [],
   /*
   ** Nuxt.js dev-modules
   */
   buildModules: [
-    '@nuxtjs/tailwindcss'
+    '@nuxtjs/tailwindcss',
   ],
   /*
   ** Nuxt.js modules
@@ -46,13 +43,24 @@ export default {
   modules: [
     // Doc: https://axios.nuxtjs.org/usage
     '@nuxtjs/axios',
+    ['@nuxtjs/google-analytics', {
+      id: ''
+    }],
+    ['@nuxtjs/yandex-metrika', {
+      id: '',
+      webvisor: true,
+      // clickmap:true,
+      // useCDN:false,
+      // trackLinks:true,
+      // accurateTrackBounce:true,
+    }
+    ],
   ],
   /*
   ** Axios module configuration
   ** See https://axios.nuxtjs.org/options
   */
-  axios: {
-  },
+  axios: {},
   /*
   ** Build configuration
   */
@@ -60,7 +68,7 @@ export default {
     /*
     ** You can extend webpack config here
     */
-    extend (config, ctx) {
+    extend(config, ctx) {
     }
   }
 }
